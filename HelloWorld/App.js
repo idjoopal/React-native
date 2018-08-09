26ad33ba61d17bfa8eaf00fa1bd9b44ffa35ee35
김주현 <idjoopal@naver.com>
2018-08-09 16:58:16 +0900

    practice TouchableHighlight

--- HelloWorld/App.js
+++ HelloWorld/App.js
@@ -1,49 +1,72 @@
 /**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+* Sample React Native App
+* https://github.com/facebook/react-native
+*
+* @format
+* @flow
+*/
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, TouchableHighlight, Text, View} from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
 });
 
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>오토 리로딩인가여?</Text>
-        <Text style={styles.instructions}>This is autoreload</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+      <View style={viewStyles}>
+      <TouchableHighlight style ={[buttonStyles.core, buttonStyles.spacer]}>
+      <Text>Default Normal</Text>
+      </TouchableHighlight>
+      
+      <TouchableHighlight style ={[buttonStyles.core, buttonStyles.hairlineBorder, buttonStyles.spacer]}>
+      <Text>Default Hairline</Text>
+      </TouchableHighlight>
+      
+      <TouchableHighlight style ={[buttonStyles.core, buttonStyles.primary, buttonStyles.spacer]}>
+      <Text>Primary Normal</Text>
+      </TouchableHighlight>
+      
+      <TouchableHighlight style ={[buttonStyles.core, buttonStyles.primary, buttonStyles.hairlineBorder]}>
+      <Text>Primary Hairline</Text>
+      </TouchableHighlight>
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+
+const viewStyles ={
+  flex: 1,
+  justifyContent: 'center',
+  alignItems: 'center'
+};
+
+const buttonStyles = StyleSheet.create({
+  core: {
+    borderStyle: 'solid',
+    borderColor: '#d5d5d5',
+    borderWidth: 1,
+    backgroundColor: '#eee',
+    borderRadius:3,
+    padding:3,
+    paddingLeft:5,
+    paddingRight:5
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  primary: {
+    backgroundColor: '#60b044',
+    borderColor: '#355f27',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  hairlineBorder: {
+    borderWidth: StyleSheet.hairlineWidth
   },
+  spacer:{
+    marginBottom:10
+  }
 });
